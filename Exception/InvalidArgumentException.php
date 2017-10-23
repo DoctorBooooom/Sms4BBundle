0029e229980a1f2c212783e85519f76de4781226
DoctorBooooom <artem-s86@yandex.ru>
2017-10-23 11:02:42 +0300

    Sms4BBundle

---
+++ Exception/InvalidArgumentException.php
@@ -0,0 +1,16 @@
+<?php
+/*
+ * This file is part of the Sms4BBundle package.
+ *
+ * (c) DoctorBooooom <https://github.com/DoctorBooooom>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Sms4BBundle\Exception;
+
+class InvalidArgumentException extends \InvalidArgumentException
+{
+
+}
